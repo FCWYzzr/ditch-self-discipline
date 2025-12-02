084ae8c0f4427354f46ac34c9eadd3ff3b06ddb8
Zheng Sun <fcwyzzr@163.com>
2025-12-02 14:52:16 +0800

    fix prefix

--- vite.config.ts
+++ vite.config.ts
@@ -6,5 +6,5 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue(), tailwindcss()],
-  base: '/dont-self-discipline/',
+  base: '/ditch-self-discipline/',
 })
